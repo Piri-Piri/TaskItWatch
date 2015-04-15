58d7ec3871b1a33c4befc8a2a36c9120edde232e
Piri-Piri <david.pirih@gmx.de>
2015-04-15 23:38:24 +0200

    Initial Commit

---
+++ TaskItWatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskItWatch
+//
+//  Created by David Pirih on 15.04.15.
+//  Copyright (c) 2015 Piri-Piri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
